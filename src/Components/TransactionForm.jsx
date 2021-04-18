1978a8dba1926f9dcda0deb0ae3e398c925a7f50
Bara Abdullatif <bara.abd@yahoo.com>
2021-04-18 14:04:08 +0200

    Create input items and event to send data to Localstorage

--- src/Components/TransactionForm.jsx
+++ src/Components/TransactionForm.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class TransactionForm extends Component {
+    state = {
+        bAccountNo: '',
+        iFCS: '',
+        bName: '',
+        amount: ''
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.onAddOrEdit(this.state)
+    }
+    
     render() {
         return (
-            <div>
-                Form for crud operations
-            </div>
+           <form onSubmit= {this.handleSubmit} autoComplete='off'>
+               <input name='bAccountNo' placeholder='A/C No.' value={this.state.bAccountNo} onChange={this.handleInputChange} /><br/>
+               <input name='iFCS' placeholder='iFCS Code' value={this.state.iFCS} onChange={this.handleInputChange} /><br/>
+               <input name='bName' placeholder='A/C Holder Name' value={this.state.bName} onChange={this.handleInputChange} /><br/>
+               <input name='amount' placeholder='Amount' value={this.state.amount} onChange={this.handleInputChange} /><br/>
+               <button name='submit'>Submit</button>
+
+
+
+           </form>
         );
     }
 }
--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import TransactionForm from './TransactionForm';
 
 class TransactionList extends Component {
+    state = {
+        list: this.returnList
+    }
+
+    returnList = () => {
+        if(localStorage.getItem('transactiona') == null)
+        localStorage.setItem('transactions', JSON.stringify([]))
+        return JSON.parse(localStorage.getItem('transactions'))
+    }
+
+    onAddOrEdit =(data) =>{
+        var list = this.returnList()
+        list.push(data)
+        localStorage.setItem('transactions', JSON.stringify(list))
+        this.setState({list})
+
+    }
+
+
     render() {
         return (
             <div>
-                <TransactionForm />
+                <TransactionForm 
+                    onAddOrEdit={this.onAddOrEdit}
+                />
                 <hr />
                 <p>List of transaction</p>
             </div>
